55b42b17cd2ac8425f3a75bb6ad7f2fcbc19f51c
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-17 04:47:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,15 @@
-
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
   
 def list_1(hash)
   hash.each do |person, data|
